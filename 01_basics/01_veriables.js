e2b9426d628191edcf787be15d470b22d158b9e7
Rutvik222 <117149249+Rutvik222@users.noreply.github.com>
2024-08-06 12:33:01 +0000

    veriables

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "rutvik0204@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "mumbai"
+  
+ //accountId = 2// not allowed
+
+ 
+ accountEmail = "hc@gmail.com"
+  accountPassword = "1212112"
+  accountCity = "Pune"
+
+  
+  console.table([accountEmail,accountPassword,accountCity,accountId]);
+  
