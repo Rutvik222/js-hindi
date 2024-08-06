1059847850463b32699750915f2211a93a4622e7
Rutvik222 <117149249+Rutvik222@users.noreply.github.com>
2024-08-06 13:16:26 +0000

    02_datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+console.log("Rutvik");
+
+let name ="Rutvik";
+
+let age = "24";;
+
+let tempreture = null;
+
+let isLoggedIn = true;
+
+
+
+// number => 2 to the power of 53
+// bigint  => used for renge of value like 12132424242424242424 it is mostly like some big companies like facebook, google so many..
+// string =>  in word format "abcd"
+// boolean => true or false 
+// null => standalone value ("null")
+// undifined =>   not value  assigned 
+// symbol => used like react (unique)
+
+console.log(typeof "Rutvik");
+
+console.log(typeof 123455);
+
+console.log(typeof null);
+
+
+console.log(typeof undifined );
